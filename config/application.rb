c779dc0d546ef7e1dbca8e251a692d9417d8834f
Nicholas Kirkwood <nkirkwood@localdirectories.com.au>
2015-10-01 11:36:08 +1000

    assets
    - set public directory as assets
    - moved css into assets directory
    - amended layout to reflect new location of assets

--- config/application.rb
+++ config/application.rb
@@ -4,4 +4,5 @@ require 'bundler'
 Bundler.require
 $: << File.expand_path('../', __FILE__)
 set :root, Dir['./app']
+set :public_folder, Proc.new { File.join(root, 'assets') }
 Dir['./app/**/*.rb'].sort.each { |file| require file }
