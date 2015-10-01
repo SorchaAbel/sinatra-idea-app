8e58b846a6e7f43c54475e421123c528140dca20
Nicholas Kirkwood <nkirkwood@localdirectories.com.au>
2015-10-01 14:24:22 +1000

    cleanup migrations

--- db/migrate/20150924112559_remove_id_idea_from_comments.rb
+++
@@ -1,8 +0,0 @@
-class RemoveIdIdeaFromComments < ActiveRecord::Migration
-  def self.up
-    remove_column :comments, :id_idea
-  end
-  def self.down
-    add_column :comments, :id_idea
-  end
-end
