eaf79e1a8aecd89d4aba5e25ab98fd3a1dc56177
Nicholas Kirkwood <nkirkwood@localdirectories.com.au>
2015-10-01 13:58:32 +1000

    formatting and readme

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,10 @@ require 'bundler'
 
 # Setup load paths
 Bundler.require
+
 $: << File.expand_path('../', __FILE__)
+Dir['./app/**/*.rb'].sort.each { |file| require file }
+
+# sinatra configuration
 set :root, Dir['./app']
 set :public_folder, Proc.new { File.join(root, 'assets') }
-Dir['./app/**/*.rb'].sort.each { |file| require file }
